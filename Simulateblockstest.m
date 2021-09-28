9362a2962ee018cfe92b9b37a6783a2b2523a651
TimKaasjager <tim.kaasjager.sbt21@gmail.com>
2021-09-28 12:34:26 +0200

    first attempt to make block appear and move down the frame for each period of the timer. Also added that it reappears at the top if it gets to the bottom

---
+++ Simulateblockstest.m
@@ -0,0 +1,11 @@
+function Simulateblockstest(app)
+step = 20;
+if app.Image2.Position(2) < 10
+    app.Image2.Position = [75 372 100 100];
+end
+app.Image2.Visible = 'on';
+app.Image3.Visible = 'off';
+app.Image4.Visible = 'off';
+app.Image2.Position = app.Image2.Position + [0 step 0 0];
+
+end
