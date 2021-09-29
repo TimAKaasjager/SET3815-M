c71bc2b324c309a7d53004b82cd7d616408b392d
TimKaasjager <tim.kaasjager.sbt21@gmail.com>
2021-09-29 15:34:49 +0200

    fixed issue with not starting

--- tryout_blocks.m
+++ tryout_blocks.m
@@ -1,14 +1,15 @@
+%https://www.youtube.com/watch?v=taDSfV2a3J8&ab_channel=SilasHenderson;
+
+%--------------------------Setup---------------------------------------%
 FallingBlocksFigure = figure('color',[.2 .8 .8],...            %Block
     'KeyPressFcn', @keyboardFunction);
 
 FallingBlocksAxes = axes('color', 'black',...                  %Axes
    'XLim', [0 100], 'YLim', [0 100]);
 
-FallingBlockVel = [0, -3];                                       %Ball
-FallingBlockPos = [20, 95];
+FallingBlockVel = [0, -2];                                       %Ball
+FallingBlockPos = [20, 70];
 FallingBlock = line(FallingBlockPos(1),FallingBlockPos(2),...  
-    'marker','.','markersize', 25,'color','red');
-FallingBlock2 = line(FallingBlockPos(1),FallingBlockPos(2),...  
     'marker','.','markersize', 25,'color','red');
 
 global PlayerCenter;                                            %Player
@@ -16,19 +17,19 @@ PlayerCenter = 45;
 Player = line([PlayerCenter - 5, PlayerCenter + 5],[5 5],...     
     'color', 'green', 'linewidth', 4);
 
-
-
 %------------------------Loop-----------------------------------------%
 tic;
 toc;
-while toc < 50
-    if FallingBlockPos(2) < 5 && abs(FallingBlockPos(1) - PlayerCenter) > 5         %
+while toc < 15
+    
+    if FallingBlockPos(2) < 5
+        if abs(FallingBlockPos(1) - PlayerCenter) > 5
             newpos = randi([10 90]);
             FallingBlockPos = [newpos 80];
-    else 
-       close all; return ;
+        else 
+           close all; return ;
+        end
     end
-    
                 
     FallingBlockPos = FallingBlockPos + FallingBlockVel;      %Update Ball  
     set(FallingBlock, 'XData', FallingBlockPos(1), 'YData', FallingBlockPos(2))
@@ -52,6 +53,5 @@ switch event.Key
 end 
 %set(gcf,'color',rand(1,3))
 end
-
 
 
