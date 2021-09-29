d7ce337f58c5044b8701d9b0560f20ada76c067b
TimKaasjager <tim.kaasjager.sbt21@gmail.com>
2021-09-29 15:13:51 +0200

    ball goes straight down and returns to top randomly

--- tryout_blocks.m
+++ tryout_blocks.m
@@ -1,16 +1,14 @@
-
-%https://www.youtube.com/watch?v=taDSfV2a3J8&ab_channel=SilasHenderson;
-
-%--------------------------Setup---------------------------------------%
 FallingBlocksFigure = figure('color',[.2 .8 .8],...            %Block
     'KeyPressFcn', @keyboardFunction);
 
 FallingBlocksAxes = axes('color', 'black',...                  %Axes
    'XLim', [0 100], 'YLim', [0 100]);
 
-FallingBlockVel = [1, 1];                                       %Ball
-FallingBlockPos = [20, 50];
+FallingBlockVel = [0, -3];                                       %Ball
+FallingBlockPos = [20, 95];
 FallingBlock = line(FallingBlockPos(1),FallingBlockPos(2),...  
+    'marker','.','markersize', 25,'color','red');
+FallingBlock2 = line(FallingBlockPos(1),FallingBlockPos(2),...  
     'marker','.','markersize', 25,'color','red');
 
 global PlayerCenter;                                            %Player
@@ -18,24 +16,19 @@ PlayerCenter = 45;
 Player = line([PlayerCenter - 5, PlayerCenter + 5],[5 5],...     
     'color', 'green', 'linewidth', 4);
 
+
+
 %------------------------Loop-----------------------------------------%
 tic;
 toc;
-while toc < 15
+while toc < 50
+    if FallingBlockPos(2) < 5 && abs(FallingBlockPos(1) - PlayerCenter) > 5         %
+            newpos = randi([10 90]);
+            FallingBlockPos = [newpos 80];
+    else 
+       close all; return ;
+    end
     
-    if FallingBlockPos(1) < 0 || FallingBlockPos(2) > 100     %Bounce check
-        FallingBlockVel(1) = -FallingBlockVel(1);
-    end
-    if FallingBlockPos(2) < 0 || FallingBlockPos(2) > 100
-        FallingBlockVel(2) = -FallingBlockVel(2);
-    end
-    if FallingBlockPos(2) < 5
-        if abs(FallingBlockPos(1) - PlayerCenter) < 5
-            FallingBlockVel(2) =  -FallingBlockVel(2)
-        else 
-           close all; return ;
-        end
-    end
                 
     FallingBlockPos = FallingBlockPos + FallingBlockVel;      %Update Ball  
     set(FallingBlock, 'XData', FallingBlockPos(1), 'YData', FallingBlockPos(2))
