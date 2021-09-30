5e58d7eff7c8957f6019e678a41c66c3c3ad9791
sophievankleef990 <sophie.vankleef@live.nl>
2021-09-30 11:05:47 +0200

    De snelheid van het groene bolletje random gemaakt en de achtergrond kleur (tijdelijk) aangepast zodat alle zwarte letters van de counters te zien zullen zijn.

--- tryout_blocks.m
+++ tryout_blocks.m
@@ -1,14 +1,15 @@
 %https://www.youtube.com/watch?v=taDSfV2a3J8&ab_channel=SilasHenderson;
 
 %--------------------------Setup---------------------------------------%
-FallingBlocksFigure = figure('color',[.2 .8 .8],...            %Block
+FallingBlocksFigure = figure('color',[0 0.4470 0.7410],...            %Block
     'KeyPressFcn', @keyboardFunction,'unit','normal','position',[.1 .1 .8 .8]);
 
-FallingBlocksAxes = axes('color', 'black',...                  %Axes
+FallingBlocksAxes = axes('color', '[0.3010 0.7450 0.9330]',...                  %Axes
    'XLim', [0 100], 'YLim', [0 100], 'XTickLabels',[],'YTickLabels',[],'position',[.05 .05 .9 .9]);
 
+giftspeed = randi([-3 -1]);
 FallingBlockVel = [0, -1];                                     %Block
-GoodBlockVel = [0, -2];                                        %Good Block
+GoodBlockVel = [0, giftspeed];                                        %Good Block
 FallingBlockPos = [30, 70];
 GoodBlockPos = [20, 90];
 FallingBlock = line(FallingBlockPos(1),FallingBlockPos(2),...  
@@ -16,14 +17,16 @@ FallingBlock = line(FallingBlockPos(1),FallingBlockPos(2),...
 GoodBlock = line(GoodBlockPos(1),GoodBlockPos(2),...  
     'marker','.','markersize', 25,'color','green');
 
-
+% scorecounter = [];
+% livescounter = [];
 
 global PlayerCenter;                                           %Player
 PlayerCenter = 45;
 Player = line([PlayerCenter - 5, PlayerCenter + 5],[5 5],...     
-    'color', 'green', 'linewidth', 4);
+    'color', '[0.5 0.5 0.5]', 'linewidth', 4);
 
 %------------------------Loop-----------------------------------------%
+
 tic;
 toc;
 while toc < 50
@@ -35,7 +38,7 @@ while toc < 50
             FallingBlockPos = [newposx newposy];
             pause(randi(3));
         else 
-           close all; return ;
+           close all; return;
         end
     end
     
@@ -48,6 +51,7 @@ while toc < 50
            close all; return;
         end
     end
+
                 
     FallingBlockPos = FallingBlockPos + FallingBlockVel;      %Update Ball  
     set(FallingBlock, 'XData', FallingBlockPos(1), 'YData', FallingBlockPos(2))
@@ -72,7 +76,7 @@ switch event.Key
     case 'd'
         PlayerCenter = PlayerCenter + 2;
 end 
-set(gcf,'color',rand(1,3))
+%set(gcf,'color',rand(1,3))
 end
 
 
