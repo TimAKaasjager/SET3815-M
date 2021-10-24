500f416fed776f523a72ee49cbbc5c8fd1bfc05e
OlavEringfeld <91196670+OlavEringfeld@users.noreply.github.com>
2021-10-24 19:44:38 +0200

    dimensions

--- tryout_blocks.m
+++ tryout_blocks.m
@@ -8,48 +8,64 @@
 %--------------------Setting up board and objects---------------%
 clear all
 FallingBlocksFigure = figure('color',[0 0.4470 0.7410],...            %Block
-    'KeyPressFcn', @keyboardFunction,'unit','normal','position',[.1 .1 .8 .8]);
+    'KeyPressFcn', @keyboardFunction,'unit','normal','position',[.1 .1 .7 .7]);
+% 
+%  FallingBlocksAxes = axes('color', 'black',...                  %Axes
+%     'XLim', [0 1280], 'YLim', [0 1280], 'XTickLabels',[],'YTickLabels',[],'position',[.05 .05 .9 .9]);
 
-% FallingBlocksAxes = axes('color', 'black',...                  %Axes
-%    'XLim', [0 100], 'YLim', [0 100], 'XTickLabels',[],'YTickLabels',[],'position',[.05 .05 .9 .9]);
-
-FallingBlocksAxes = gca';
+FallingBlocksAxes = gca;
 uistack(FallingBlocksAxes,'bottom');
-[x,FallingBlocksAxes]=imread('clouds.jpg');
+[x, FallingBlocksAxes]=imread('clouds2.jpg');
 image(x)
+%set(x, 'ydir','reverse')
 set(FallingBlocksAxes,'handlevisibility','off','visible','off')
 set(FallingBlocksAxes,'alphadata',.05)
 ax = gca;
-ax.YDir = 'normal';
-set(gca,'visible','off');
+ax.YDir = 'normal'
+set(gca,'visible','off')
+%set(gca, 'ydir','reverse')
+%set(FallingBlocksAxes,'ydir','reverse')
+%set(gca, 'YDir','reverse')
+%set(FallingBlocksAxes, 'YDir','reverse')
+
+
 
 %defining initial velocities and positions
-FallingBlockVel = [0, -1];                                      %Block
-FallingBlockvelsq = [0, -1];                                    %Block 2
-GoodBlockVel = [0, -1];                                        %Good Block
-FallingBlockVelpl = [0 0; -1 -1];                               %plastic
+FallingBlockVel = [0, -12.8]; %[0, -1];                                      %Block
+FallingBlockvelsq = [0, -12.8]; %[0, -1];                                   %Block 2
+GoodBlockVel = [0, -12.8];   %[0, -1];                                     %Good Block
+FallingBlockVelpl = [0 0; -12.8 -12.8]; %[0 0; -1 -1];                               %plastic
 
-FallingBlockPos = [30 70];
-FallingBlockPossq = [50 80];
-FallingBlockPoswi = [50 60;70 80];
-GoodBlockPos = [20, 90];
+FallingBlockPos = [384 896]; %[30 70];
+FallingBlockPossq = [640 1024]; %[50 80];
+FallingBlockPoswi = [640 768;896 1024]; %[50 60;70 80];
+GoodBlockPos = [256, 1152]; %[20, 90];
 
 %defining shapes of player and falling blocks, including implementing image of plastic
 %bag
+
 FallingBlock = line(FallingBlockPos(1),FallingBlockPos(2),...  
     'marker','.','markersize', 50,'color','red');
 FallingBlocksq = line(FallingBlockPossq(1),FallingBlockPossq(2),...  
     'marker','s','markersize', 30,'color','red');
+
+%  solar = imread('solar1.png'); 
+%  solar = imresize(solar, 1);
+ 
+ % fallingplastic = image('XData',FallingBlockPossq(1),'YData',FallingBlockPossq(2),'CData', flipud(solar));
+
 %---Format for including blocks
-% wind = imread(); 
-% wind = imresize(wind, 0.01);
-% fallingwind = image('XData',FallingBlockPoswi(1,:),'YData',FallingBlockPoswi(2,:),'CData', flipud(wind)); 
-GoodBlock = line(GoodBlockPos(1),GoodBlockPos(2),...  
+%  FallingBlocksq = imread('solar1.png');
+%  imshow('solar1.png')
+%  FallingBlocksq = imresize(FallingBlocksq, 0.1);
+%  fallingwind = image('XData',FallingBlockPoswi(1,:),'YData',FallingBlockPoswi(2,:),'CData', flipud(wind)); 
+
+ GoodBlock = line(GoodBlockPos(1),GoodBlockPos(2),...  
     'marker','.','markersize', 25,'color','green');
 
 global PlayerCenter;                                           %Player
-PlayerCenter = 45;
-Player = line([PlayerCenter - 5, PlayerCenter + 5],[5 5],...     
+PlayerCenter = 576;
+Player = line([PlayerCenter - 64, PlayerCenter + 64],[64 64],...     
     'color', '[0.5 0.5 0.5]', 'linewidth', 4);
 
 %% 
@@ -62,12 +78,12 @@ pointscounter = 0;
 Display_lives = 'Lives';
 amount_lives = livescounter;
 X = [Display_lives,' = ',num2str(livescounter)];
-T_lives = text(80,80,X,'FontSize',40,'Color','w','FontName','Impact');
+T_lives = text(1024,1024,X,'FontSize',40,'Color','w','FontName','Impact');
 
 Display_points = 'Points';
 amount_points = pointscounter;
 Y = [Display_points, ' = ', num2str(pointscounter)];
-T_points = text(80,70,Y,'Fontsize',40,'Color','w','FontName','Impact');
+T_points = text(1024,896,Y,'Fontsize',40,'Color','w','FontName','Impact');
 
 tic;
 toc;
@@ -79,34 +95,35 @@ while toc < inf                 %game keeps running indefinitely
    [FallingBlockPossq, FallingBlockVel, livescounter] = Badblock(FallingBlockPossq,FallingBlockVel,livescounter,PlayerCenter);
     %Calls a different function for the good block, as hitting the player
     %has different consequences than hitting a bad block
+   
    [GoodBlockPos, GoodBlockVel, pointscounter] = Goodblockfunc(GoodBlockPos,GoodBlockVel,pointscounter,PlayerCenter);
-if livescounter == 0                            %what happens when the game is over
+%       solar = imread('solar1.png'); 
+%       solar = imresize(solar, 1);
+%       solarpic = image('XData',FallingBlockPossq(1),'YData',FallingBlockPossq(2),'CData', flipud(solar));
+
+   if livescounter == 0                            %what happens when the game is over
 %     bar(pointscounter)
 %     text(1:length(pointscounter),pointscounter,num2str(pointscounter'),'vert','bottom','horiz','center'); 
 %     set(gca,'XTick',[], 'YTick', [])
 %     title('Your Score','FontSize',10)
 %     %Xlabel('Your Score')
 
-    gameover = text(20,50,'GAME OVER','FontSize',...    %GAME OVER text appears
+    gameover = text(256,640,'GAME OVER','FontSize',...    %GAME OVER text appears
     100,'Color','r','FontName','Impact');
-    T_points = text(20,40,Y,'Fontsize',40,...           %redefined because setting x and y values doesn't work
+    T_points = text(256,512,Y,'Fontsize',40,...           %redefined because setting x and y values doesn't work
     'Color','w','FontName','Impact');
     set(T_points, 'str', ['Your final score is ', num2str(pointscounter)])
     set(T_lives, 'str', [Display_lives,' = ',num2str(livescounter)])
     break
     
-end
-
+   end
 
     FallingBlockPos = FallingBlockPos + FallingBlockVel;      %add velocity step to position
     set(FallingBlock, 'XData', FallingBlockPos(1), 'YData', FallingBlockPos(2)) %set new position
     
     FallingBlockPossq = FallingBlockPossq + FallingBlockVel;
     set(FallingBlocksq, 'XData', FallingBlockPossq(1), 'YData', FallingBlockPossq(2))
-    
-%     FallingBlockPospl = FallingBlockPospl + FallingBlockVelpl;
-%     set(fallingplastic, 'XData', FallingBlockPospl(1), 'YData', FallingBlockPospl(2))
-    
+      
     GoodBlockPos = GoodBlockPos + GoodBlockVel;
     set(GoodBlock, 'XData', GoodBlockPos(1), 'YData', GoodBlockPos(2))
     
@@ -114,25 +131,30 @@ end
     
     set(T_points, 'str', [Display_points,' = ',num2str(pointscounter)])
     
-    set(Player, 'XData', [PlayerCenter - 5, PlayerCenter + 5])
+    set(Player, 'XData', [PlayerCenter - 64, PlayerCenter + 64])
     
     pause(0.05);
 end
 
 %% 
+ 
+%  solar = imread('solar1.png'); 
+%  solar = imresize(solar, 1);
+%  solarpic = image('XData',FallingBlockPossq(1,:),'YData',FallingBlockPossq(2,:),'CData', flipud(solar));
 
-%Function
-function keyboardFunction(figure,event)
+ 
+ %Function
+function keyboardFunction(~,event)
 global PlayerCenter
 switch event.Key
     case 'rightarrow'
-        PlayerCenter = PlayerCenter + 2;
+        PlayerCenter = PlayerCenter + 25.6;
     case 'leftarrow'
-        PlayerCenter = PlayerCenter - 2;
+        PlayerCenter = PlayerCenter - 25.6;
     case 'a'
-        PlayerCenter = PlayerCenter - 2;
+        PlayerCenter = PlayerCenter - 25.6;
     case 'd'
-        PlayerCenter = PlayerCenter + 2;
+        PlayerCenter = PlayerCenter + 25.6;
 end 
 %set(gcf,'color',rand(1,3))
 end
